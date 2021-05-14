89b51a0ec6568be4081a471545bf5ff4661d8597
Philipp Harnisch <p.harnisch.privat@gmail.com>
2021-05-14 15:46:48 +0200

    .

---
+++ approach-1/NER-or-CLASS-train.py
@@ -0,0 +1,47 @@
+from flair.datasets import ColumnCorpus
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+from flair.embeddings import TransformerWordEmbeddings, FlairEmbeddings, StackedEmbeddings, WordEmbeddings
+from torch.optim.lr_scheduler import OneCycleLR
+import flair
+import torch
+
+flair.set_seed(1)
+
+task = "ner-1"  # 1 (NER): ner-1, 2 (CLASS): ner-2
+columns = {0: "text", 1: "ner-1", 2: "ner-2"}
+data_folder = "../constructed-training-data/"
+corpus = ColumnCorpus(data_folder, columns, train_file="all.txt")  #.downsample(0.1)
+
+print(f"The training corpus contains {len(corpus.train)} (pretty long) sample sentences.")
+print(f"The validation corpus contains {len(corpus.dev)} (pretty long) sample sentences.")
+print(f"The testing corpus contains {len(corpus.test)} (pretty long) sample sentences.")
+
+hidden_size = 128
+embeddings = StackedEmbeddings(embeddings=[WordEmbeddings("es"), FlairEmbeddings("es-forward"), FlairEmbeddings("es-backward")]) # TransformerWordEmbeddings()
+dictionary = corpus.make_tag_dictionary(task)
+tagger = SequenceTagger(hidden_size, embeddings, dictionary, task)
+
+trainer = ModelTrainer(tagger, corpus, optimizer=torch.optim.AdamW)
+trainer.train(
+    base_path="taggers/2-stacked",
+    train_with_dev=False,
+    max_epochs=30,
+    learning_rate=0.001,
+    mini_batch_size=32,
+    weight_decay=0.,
+    embeddings_storage_mode="none",
+    scheduler=OneCycleLR,
+)
+
+
+# trainer = ModelTrainer(tagger, corpus)
+# trainer.train(
+#     base_path="taggers/2-stacked",
+#     train_with_dev=False,
+#     max_epochs=30,
+#     learning_rate=0.1,
+#     mini_batch_size=32,
+# )
+
+# TODO: do not split data into train, val and test. only in train and val. because there will be coming extra test data without annotation!
